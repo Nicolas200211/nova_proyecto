5016f16eef57d6279493d69a9e2198c59f9a4145
Nicolas200211 <nico_e123@outlook.com>
2025-07-28 19:26:18 -0500

    confi2

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const isProduction = command === 'build';
-  
-  return {
-    base: isProduction ? '/my_page_web/' : '/',
-    plugins: [
-      tailwindcss(),
-      react()
-    ],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src')
-      }
+export default defineConfig({
+  base: '/my_page_web/',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-      outDir: 'dist',
-      sourcemap: true,
-      minify: 'terser',
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            react: ['react', 'react-dom', 'react-router-dom'],
-            vendor: ['framer-motion', 'gsap', 'three']
-          }
-        }
-      }
-    },
-    server: {
-      port: 3000,
-      open: true
-    }
-  }
-})
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
