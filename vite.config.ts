dceeb3ea398e752e1697d46f6639287673ad3c04
Nicolas200211 <nico_e123@outlook.com>
2025-07-28 19:47:55 -0500

    Actualizar configuracion para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my_page_web/',
+  base: '/nova_proyecto/',
   plugins: [
     tailwindcss(),
     react()
